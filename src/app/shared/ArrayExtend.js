869378bc40f26fd86af5c8c027f91265d9a37fa9
Panchal, Pushpakkumar <pushpak.net@gmail.com>
2020-04-03 19:01:48 -0500

    adding js file

---
+++ src/app/shared/ArrayExtend.js
@@ -0,0 +1,15 @@
+let a = [1,2,3,4,5,6]
+
+   Array.prototype.pushpakmap = function(logic) {
+
+    let arr = [];
+    for (let index = 0; index < this.length; index++) {
+            const altered = logic(this[index]);
+
+              arr.push(altered);      
+      }
+
+      return arr;
+    };
+
+   
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -10,6 +10,10 @@ export class TestComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    
   }
+  
+
 
 }
